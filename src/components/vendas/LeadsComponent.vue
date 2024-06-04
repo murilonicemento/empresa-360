28c8d27cf3d4d33eb3682fcba2270f8a80c40027
murilonicemento <murilo_nascimento22@outlook.com>
2024-06-03 22:36:18 -0300

    feat: list register

--- src/components/vendas/LeadsComponent.vue
+++ src/components/vendas/LeadsComponent.vue
@@ -1,1 +1,48 @@
-<template><h5>Leads</h5></template>
+<template>
+  <div>
+    <h5>Leads</h5>
+    <table class="table table-hover">
+      <thead>
+        <tr>
+          <th scope="col">ID</th>
+          <th scope="col">NOME</th>
+          <th scope="col">TELEFONE</th>
+          <th scope="col"></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="d in dados" :key="d.id">
+          <td>{{ d.id }}</td>
+          <td>{{ d.nome }}</td>
+          <td>{{ d.telefone }}</td>
+          <td>
+            <routerLink to="/home/vendas/leads" class="btn btn-sm btn-primary">
+              <i class="bi bi-pencil-square" style="cursor: pointer"></i>
+            </routerLink>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LeadsComponent",
+  data: () => ({
+    dados: null,
+  }),
+  methods: {
+    getDadosApi() {
+      fetch("http://localhost:3000/leads")
+        .then((response) => response.json())
+        .then((response) => {
+          this.dados = response;
+        });
+    },
+  },
+  created() {
+    this.getDadosApi();
+  },
+};
+</script>
