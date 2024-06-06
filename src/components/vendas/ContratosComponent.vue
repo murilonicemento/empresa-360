a83ac82c47fd999b78ebea6a46364dfff8a9a42d
murilonicemento <murilo_nascimento22@outlook.com>
2024-06-05 22:04:38 -0300

    feat: list register

--- src/components/vendas/ContratosComponent.vue
+++ src/components/vendas/ContratosComponent.vue
@@ -1,1 +1,37 @@
-<template><h5>Contratos</h5></template>
+<template>
+  <div>
+    <h5>Contratos</h5>
+    <table class="table table-hover">
+      <thead>
+        <tr>
+          <th scope="col">ID</th>
+          <th scope="col">LEAD</th>
+          <th scope="col">SERVIÇO</th>
+          <th scope="col">DATA INÍCIO</th>
+          <th scope="col">DATA FIM</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="d in dados" :key="d.id">
+          <td>{{ d.id }}</td>
+          <td>{{ d.leadId }}</td>
+          <td>{{ d.servicoId }}</td>
+          <td>{{ d.data_inicio }}</td>
+          <td>{{ d.data_fim }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import ApiMixin from "@/mixins/ApiMixin";
+
+export default {
+  name: "ContratosComponent",
+  mixins: [ApiMixin],
+  created() {
+    this.getDadosApi("http://localhost:3000/contratos");
+  },
+};
+</script>
