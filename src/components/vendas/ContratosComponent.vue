efb4e454644948f0b8b521339673f26f7da6e968
murilonicemento <murilo_nascimento22@outlook.com>
2024-06-05 22:59:27 -0300

    feat: query params with router link

--- src/components/vendas/ContratosComponent.vue
+++ src/components/vendas/ContratosComponent.vue
@@ -1,6 +1,26 @@
 <template>
   <div>
     <h5>Contratos</h5>
+    <routerLink
+      class="btn btn-primary"
+      :to="{ name: 'contratos', query: { leadId_like: 1 } }"
+      >LeadId = 1</routerLink
+    >
+    <routerLink
+      class="btn btn-primary"
+      to="/home/vendas/contratos?servicoId_like=2"
+      >ServicoId = 2</routerLink
+    >
+    <routerLink
+      class="btn btn-success"
+      :to="{ name: 'contratos', query: { leadId_like: 1, servicoId_like: 2 } }"
+      >leadId = 1 e servicoId = 2</routerLink
+    >
+    <routerLink
+      class="btn btn-success"
+      to="/home/vendas/contratos?servicoId_like=2&leadId_like=2"
+      >ServicoId = 2 e leadId = 2</routerLink
+    >
     <table class="table table-hover">
       <thead>
         <tr>
@@ -14,8 +34,8 @@
       <tbody>
         <tr v-for="d in dados" :key="d.id">
           <td>{{ d.id }}</td>
-          <td>{{ d.leadId }}</td>
-          <td>{{ d.servicoId }}</td>
+          <td>{{ d.lead.nome }}</td>
+          <td>{{ d.servico.servico }}</td>
           <td>{{ d.data_inicio }}</td>
           <td>{{ d.data_fim }}</td>
         </tr>
@@ -30,8 +50,19 @@ import ApiMixin from "@/mixins/ApiMixin";
 export default {
   name: "ContratosComponent",
   mixins: [ApiMixin],
+  data: () => ({
+    parametroDeRelacionamento: "_expand=lead&_expand=servico",
+  }),
   created() {
-    this.getDadosApi("http://localhost:3000/contratos");
+    this.getDadosApi(
+      `http://localhost:3000/contratos?${this.parametroDeRelacionamento}`
+    );
+  },
+  beforeRouteUpdate(to, from, next) {
+    const queryParams = new URLSearchParams(to.query).toString();
+    const url = `http://localhost:3000/contratos?${this.parametroDeRelacionamento}&${queryParams}`;
+    this.getDadosApi(url);
+    next();
   },
 };
 </script>
