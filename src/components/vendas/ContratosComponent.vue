ecc8d13d4e050ce4f86d96cf10985c85dfe3a90e
murilonicemento <murilo_nascimento22@outlook.com>
2024-06-06 23:00:20 -0300

    feat: using query params with programmatic navegation

--- src/components/vendas/ContratosComponent.vue
+++ src/components/vendas/ContratosComponent.vue
@@ -1,26 +1,40 @@
 <template>
   <div>
-    <h5>Contratos</h5>
-    <routerLink
-      class="btn btn-primary"
-      :to="{ name: 'contratos', query: { leadId_like: 1 } }"
-      >LeadId = 1</routerLink
-    >
-    <routerLink
-      class="btn btn-primary"
-      to="/home/vendas/contratos?servicoId_like=2"
-      >ServicoId = 2</routerLink
-    >
-    <routerLink
-      class="btn btn-success"
-      :to="{ name: 'contratos', query: { leadId_like: 1, servicoId_like: 2 } }"
-      >leadId = 1 e servicoId = 2</routerLink
-    >
-    <routerLink
-      class="btn btn-success"
-      to="/home/vendas/contratos?servicoId_like=2&leadId_like=2"
-      >ServicoId = 2 e leadId = 2</routerLink
-    >
+    <div class="card mb-4">
+      <div class="card-header">Contratos</div>
+      <div class="card-body">
+        <div class="row">
+          <div class="col-6">
+            <label class="form-label">ID Contrato:</label>
+            <input
+              type="text"
+              class="form-control"
+              v-model="formPesquisa.id_like"
+            />
+          </div>
+          <div class="col-6">
+            <label class="form-label">Data início:</label>
+            <div class="input-group">
+              <input
+                type="date"
+                class="form-control"
+                v-model="formPesquisa.data_inicio_get"
+              />
+              <input
+                type="date"
+                class="form-control"
+                v-model="formPesquisa.data_fim_lte"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="card-footer">
+        <button type="button" class="btn btn-primary" @click="pesquisar()">
+          Pesquisar
+        </button>
+      </div>
+    </div>
     <table class="table table-hover">
       <thead>
         <tr>
@@ -52,7 +66,22 @@ export default {
   mixins: [ApiMixin],
   data: () => ({
     parametroDeRelacionamento: "_expand=lead&_expand=servico",
+    formPesquisa: {
+      id_like: "",
+      data_inicio_get: "",
+      data_fim_lte: "",
+    },
   }),
+  methods: {
+    pesquisar() {
+      Object.keys(this.formPesquisa).forEach((chave) => {
+        if (this.formPesquisa[chave] == "") delete this.formPesquisa[chave];
+      });
+      const queryParams = new URLSearchParams(this.formPesquisa).toString();
+      const url = `http://localhost:3000/contratos?${this.parametroDeRelacionamento}&${queryParams}`;
+      this.getDadosApi(url);
+    },
+  },
   created() {
     const queryParams = new URLSearchParams(this.$route.query).toString();
     this.getDadosApi(
