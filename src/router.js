81fa9b03812caf72156af4e61765e73b1507edb1
murilonicemento <murilo_nascimento22@outlook.com>
2024-06-12 13:30:09 -0300

    feat: scroll control and lazy loading

--- src/router.js
+++ src/router.js
@@ -1,117 +1,181 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from "@/views/HomeView.vue";
-import LoginView from "@/views/LoginView.vue";
-import SiteView from "@/views/SiteView.vue";
-import VendasComponent from "@/components/vendas/VendasComponent.vue";
-import VendasPadrao from "@/components/vendas/VendasPadrao.vue";
-import LeadsComponent from "@/components/vendas/LeadsComponent.vue";
-import LeadComponent from "@/components/vendas/LeadComponent.vue";
-import ContratosComponent from "@/components/vendas/ContratosComponent.vue";
-import ServicosComponent from "@/components/servicos/ServicosComponent.vue";
-import ServicoComponent from "@/components/servicos/ServicoComponent.vue";
-import DashboardComponent from "@/components/dashboard/DashboardComponent.vue";
-import DashboardRodape from '@/components/dashboard/DashboardRodape.vue';
-import OpcoesComponent from './components/servicos/OpcoesComponent.vue';
-import IndicadoresComponent from './components/servicos/IndicadoresComponent.vue';
-import PaginaNaoEncontrada from './views/PaginaNaoEncontrada.vue';
+//createWebHashHistory
+//import Contratos from '@/components/vendas/Contratos.vue'
+//import Dashboard from '@/components/dashboard/Dashboard.vue'
+//import DashboardRodape from '@/components/dashboard/DashboardRodape.vue'
+//import Home from '@/views/Home.vue'
+//import Indicadores from '@/components/servicos/Indicadores.vue'
+//import Lead from '@/components/vendas/Lead.vue'
+//import Leads from '@/components/vendas/Leads.vue'
+//import Login from '@/views/Login.vue'
+//import Opcoes from '@/components/servicos/Opcoes.vue'
+//import PaginaNaoEncontrada from '@/views/PaginaNaoEncontrada.vue'
+//import Servico from '@/components/servicos/Servico.vue'
+//import Servicos from '@/components/servicos/Servicos.vue'
+//import Site from '@/views/Site.vue'
+//import Vendas from '@/components/vendas/Vendas.vue'
+//import VendasPadrao from '@/components/vendas/VendasPadrao.vue'
+
+//lazy loading
+const Contratos = () => import(/* webpackChunkName: "vendas" */ '@/components/vendas/Contratos.vue');
+const Dashboard = () => import(/* webpackChunkName: "dashboard" */ '@/components/dashboard/Dashboard.vue');
+const DashboardRodape = () => import(/* webpackChunkName: "dashboard" */ '@/components/dashboard/DashboardRodape.vue');
+const Home = () => import('@/views/Home.vue');
+const Indicadores = () => import(/* webpackChunkName: "servicos" */ '@/components/servicos/Indicadores.vue');
+const Lead = () => import(/* webpackChunkName: "vendas" */'@/components/vendas/Lead.vue');
+const Leads = () => import(/* webpackChunkName: "vendas" */'@/components/vendas/Leads.vue');
+const Login = () => import('@/views/Login.vue');
+const Opcoes = () => import(/* webpackChunkName: "servicos" */ '@/components/servicos/Opcoes.vue');
+const PaginaNaoEncontrada = () => import('@/views/PaginaNaoEncontrada.vue');
+const Servico = () => import(/* webpackChunkName: "servicos" */ '@/components/servicos/Servico.vue');
+const Servicos = () => import(/* webpackChunkName: "servicos" */ '@/components/servicos/Servicos.vue');
+const Site = () => import('@/views/Site.vue');
+const Vendas = () => import(/* webpackChunkName: "vendas" */'@/components/vendas/Vendas.vue');
+const VendasPadrao = () => import(/* webpackChunkName: "vendas" */'@/components/vendas/VendasPadrao.vue');
 
 const routes = [
   {
-    path: "/",
-    component: SiteView,
+    path: '/', //localhost:8080/
+    component: Site,
     meta: { requerAutorizacao: false }
   },
   {
-    path: "/home",
-    alias: "/app",
-    component: HomeView,
+    path: '/home', //localhost:8080/home
     meta: { requerAutorizacao: true },
+    alias: '/app',
+    component: Home,
     children: [
       {
-        path: "vendas",
-        component: VendasComponent,
-        children: [
-          {
-            path: "leads",
-            component: LeadsComponent,
-            name: "leads",
-            beforeEnter(to, from, next) {
-              // Executa antes da navegação
-              console.log(to, from);
-              next();
-            }
-          },
-          {
-            path: "leads/:id", /*props: { id: 2 }, props: () => ({ id: 3 }),*/ props: true, alias: ["/l/:id", "pessoa/:id", "/:id"], component: LeadComponent, name: "lead"
-          },
-          {
-            path: "contratos", component: ContratosComponent, name: "contratos"
-          },
-          {
-            path: "", component: VendasPadrao
-          }
-        ]
-      },
+        path: 'vendas', component: Vendas, children:
+          [
+            {
+              path: 'leads',
+              component: Leads,
+              name: 'leads',
+              //beforeEnter(to, from, next) {
+              beforeEnter() {
+                //poderíamos verificar se o usuário tem permissão de carregar a rota
+                console.log('Guarda de rota beforeEnter');
+              }
+            }, //localhost:8080/home/vendas/leads
+            {
+              path: 'leads/:id/:outroParametro',
+              props: true,
+              /*
+              props: {
+                  id: 4,
+                  outroParametro: 'pt-br'
+              },
+              */
+              /*
+              props: route => {
+
+                  console.log('Rota ativa: ', route)
+
+                  let teste = route.query.idioma ? route.query.idioma : route.params.outroParametro
+
+                  //implementando uma lógica para definir as props que serão submetidas para o componente roteado
+                  return { 
+                      id: parseInt(route.params.id) + 1,
+                      outroParametro: teste 
+                  }
+              },
+              */
+              component: Lead,
+              name: 'lead',
+              alias: [
+                '/l/:id/:outroParametro',
+                '/pessoa/:id/:outroParametro',
+                '/:id/:outroParametro'
+              ]
+            }, //localhost:8080/5
+            { path: 'contratos', component: Contratos, name: 'contratos' }, //localhost:8080/home/vendas/contratos
+            { path: '', component: VendasPadrao, name: 'vendas' } //localhost:8080/home/vendas/
+
+          ]
+      }, //localhost:8080/home/vendas
       {
-        path: "servicos", component: ServicosComponent, name: "servicos", children: [
-          {
-            path: ":id",
-            props: {
-              default: true,
-              indicadores: false,
-              opcoes: true,
-            },
-            alias: "/s/:id",
-            components: {
-              default: ServicoComponent,
-              opcoes: OpcoesComponent,
-              indicadores: IndicadoresComponent
-            },
-            name: "servico"
-          }
-        ]
-      },
+        path: 'servicos', component: Servicos, name: 'servicos', children:
+          [
+            {
+              path: ':id',
+              props: {
+                default: true,
+                indicadores: true,
+                opcoes: true
+              },
+              alias: '/s/:id',
+              name: 'servico',
+              components:
+              {
+                default: Servico,
+                indicadores: Indicadores,
+                opcoes: Opcoes
+              }
+            } //localhost:8080/home/servicos/5
+          ]
+      }, //localhost:8080/home/servicos
       {
-        path: "dashboard", components: {
-          default: DashboardComponent,
+        path: 'dashboard', components:
+        {
+          default: Dashboard,
           rodape: DashboardRodape
         }
-      },
+      }//localhost:8080/home/dashboard
     ]
   },
   {
-    path: "/login",
-    component: LoginView
+    path: '/login', //localhost:8080/login
+    component: Login,
+    meta: { requerAutorizacao: false, campo2: 'teste', campo3: 50 }
   },
-  { path: "/redirecionamento-1", redirect: '/home/servicos' },
-  { path: "/redirecionamento-2", redirect: { name: "leads" } },
-  { path: "/redirecionamento-3", redirect: "/home/vendas" },
+  { path: '/redirecionamento-1', redirect: '/home/servicos' },
+  { path: '/redirecionamento-2', redirect: { name: 'leads' } },
+  { path: '/redirecionamento-3', redirect: '/home/vendas' },
+  { path: '/redirecionamento-4', redirect: { name: 'vendas' } },
   {
-    path: "/redirecionamento-4", redirect: to => {
+    path: '/redirecionamento-5', redirect: to => {
+      //podemos programar algo antes do redirecionamento ser efetivado
       console.log(to);
 
-      return '/home/vendas';
+      //return '/home/vendas'
+      return { name: 'vendas' };
     }
   },
-  { path: "/:catchAll(.*)*", component: PaginaNaoEncontrada },
+  //{ path: '/:catchAll(.*)*', redirect: '/' } //Vue2 = *
+  { path: '/:catchAll(.*)*', component: PaginaNaoEncontrada } //Vue2 = *
 ];
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
+  scrollBehavior(to, from, savedPosition) {
+    //return { left: 150, top: 150 } //left = x; top = y
+    if (savedPosition) {
+      return savedPosition;
+    }
+
+    if (to.hash) {
+      return { el: to.hash }; //to.hash deve corresponder a um id de elemento HTML
+      //fragmento = #secao_1 => id = secao_1
+    }
+
+    return { left: 0, top: 0 };
+  },
+  routes //routes: routes
 });
 
-router.beforeEach((to) => {
-  // Executado antes do acesso a rota destino
-  if (to.meta.requerAutorizacao) {
-    console.log("validar acesso");
-  }
+//router.beforeEach((to, from, next) => {
+router.beforeEach(() => {
+  //console.log('Guarda global beforeEach')
 });
 
-router.afterEach((to, from) => {
-  // Executado depois do acesso a rota destino
-  console.log("Origem: ", from);
-  console.log("Destino: ", to);
+//router.afterEach((to, from) => {
+router.afterEach(() => {
+  //console.log('Guarda global afterEach')
+});
+
+router.beforeResolve(() => {
+  //console.log('Guarda global beforeResolve')
 });
 
 export default router;
